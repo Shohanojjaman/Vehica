1f909693090faa09817544e5736d2a0298ee1909
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-10-22 19:07:44 +0600

    Added container padding.

--- vehica-frontend/tailwind.config.js
+++ vehica-frontend/tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1.25rem',
+        lg: '0rem',
+      },
+    },
     extend: {
       colors: {
         primary: '#ff4605',
