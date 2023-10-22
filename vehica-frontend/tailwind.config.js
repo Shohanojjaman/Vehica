134aca091887ab7b2cb03b0845aa89c09c0982b2
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-10-22 19:01:35 +0600

    Added colors.

--- vehica-frontend/tailwind.config.js
+++ vehica-frontend/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#ff4605',
+        'primary-light': '#fff0eb',
+        heading: '#222732',
+        body: '#6f6f6f',
+      },
+    },
   },
   plugins: [],
 };
